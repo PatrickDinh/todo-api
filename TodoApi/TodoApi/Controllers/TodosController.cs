4852aace2ff3d0b740f60810541b48f8de4bc18b
Patrick Dinh <patrick.dinh@readify.net>
2018-02-03 14:44:22 +1000

    WIP - adding to do controller

--- TodoApi/TodoApi/Controllers/TodosController.cs
+++ TodoApi/TodoApi/Controllers/TodosController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Domain;
 using TodoApi.Models;
 
 namespace TodoApi.Controllers
@@ -6,10 +8,19 @@ namespace TodoApi.Controllers
     [Route("api/[controller]")]
     public class TodosController : Controller
     {
+        private readonly IReadRepository<Todo> _todoReadRepository;
+
+        public TodosController(IReadRepository<Todo> todoReadRepository)
+        {
+            _todoReadRepository = todoReadRepository;
+        }
+
         [HttpGet]
         public TodoModel[] Get()
         {
-            
+            var allTodos = _todoReadRepository.Get();
+            return allTodos.Select(td => new TodoModel())
+                .ToArray();
         }
     }
 }
